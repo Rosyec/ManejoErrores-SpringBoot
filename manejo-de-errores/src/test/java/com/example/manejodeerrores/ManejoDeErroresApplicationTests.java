c8740a3b355380943dee37998763dcb0294a4720
Rosyec <chechomens@gmail.com>
2022-10-12 10:44:30 -0500

    Creación del proyecto

---
+++ manejo-de-errores/src/test/java/com/example/manejodeerrores/ManejoDeErroresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.manejodeerrores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManejoDeErroresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
